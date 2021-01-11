f08b4954f7db8582f0e792807acd81eeb194b672
Sovent <vmargelov@gmail.com>
2021-01-11 13:51:56 +0800

    setup project

---
+++ DutyCycle.API/Controllers/GroupsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DutyCycle.API.Controllers
+{
+    [ApiController]
+    [Route("groups")]
+    public class GroupsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
