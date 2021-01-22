29a57caf10aebb2a07994487975bde2d9bac311c
Sovent <vmargelov@gmail.com>
2021-01-22 18:41:15 +0800

    add option to launch project as a worker

--- DutyCycle.API/Program.cs
+++ DutyCycle.API/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,11 +14,29 @@ namespace DutyCycle.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostBuilder = CreateHostBuilder(args);
+            
+            if (args.Contains("--worker"))
+            {
+                ConfigureServicesForWorker(hostBuilder);
+            }
+            
+            hostBuilder.Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        private static void ConfigureServicesForWorker(IHostBuilder builder)
+        {
+            builder.ConfigureServices(collection =>
+            {
+                collection.AddHangfireServer();
+            });
+        }
     }
 }
