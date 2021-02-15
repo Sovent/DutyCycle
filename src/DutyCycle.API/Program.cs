72847f66dff5676dd273791e64a018d641432fc6
Sovent <vmargelov@gmail.com>
2021-02-15 17:16:44 +0800

    add option to launch migrations from command line

--- src/DutyCycle.API/Program.cs
+++ src/DutyCycle.API/Program.cs
@@ -1,5 +1,9 @@
-using System.Linq;
+using System;
+using DutyCycle.Infrastructure.EntityFramework;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace DutyCycle.API
@@ -8,14 +12,52 @@ namespace DutyCycle.API
     {
         public static void Main(string[] args)
         {
-            var hostBuilder = CreateHostBuilder(args);
+            var commandLineApplication = new CommandLineApplication();
+            var startAsWorker = commandLineApplication.Option(
+                "--worker", 
+                "Start as background jobs processor",
+                CommandOptionType.NoValue);
+            var migrateDatabase = commandLineApplication.Option(
+                "--migrate",
+                "Update application database to the last version", 
+                CommandOptionType.NoValue);
+            commandLineApplication.HelpOption("--help");
+            commandLineApplication.OnExecute(() => ExecuteProgram(args, startAsWorker, migrateDatabase));
+
+            commandLineApplication.Execute(args);
+        }
+
+        private static int ExecuteProgram(
+            string[] args,
+            CommandOption startAsWorkerOption,
+            CommandOption migrateDatabaseOption)
+        {
+            if (startAsWorkerOption.HasValue() && migrateDatabaseOption.HasValue())
+            {
+                Console.WriteLine("Database migration is supposed to return exit code, while worker can't exit.");
+                Console.WriteLine("Can't use --worker and --migrate simultaneously");
+                return 1;
+            }
             
-            if (args.Contains("--worker"))
+            var hostBuilder = CreateHostBuilder(args);
+            if (startAsWorkerOption.HasValue())
             {
                 ConfigureServicesForWorker(hostBuilder);
             }
+
+            var host = hostBuilder.Build();
+            if (migrateDatabaseOption.HasValue())
+            {
+                Console.WriteLine("Applying database migrations");
+                using var serviceScope = host.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+                using var context = serviceScope.ServiceProvider.GetService<DutyCycleDbContext>();
+                context.Database.Migrate();
+                Console.WriteLine("Migrations finished");
+                return 0;
+            }
             
-            hostBuilder.Build().Run();
+            host.Run();
+            return 0;
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
